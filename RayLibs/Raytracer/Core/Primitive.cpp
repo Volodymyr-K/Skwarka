812fe2cbf6d92498e4aa367efa98939bc909558f
Volodymyr Kachurovskyi <Volodymyr.Kachurovskyi@gmail.com>
2010-10-27 11:31:10 +0000

    Bump mapping code rewritten.

--- RayLibs/Raytracer/Core/Primitive.cpp
+++ RayLibs/Raytracer/Core/Primitive.cpp
@@ -8,27 +8,25 @@ void Primitive::_Bump(const DifferentialGeometry &i_dg, size_t i_triangle_index,
 
   dg_eval.m_point = i_dg.m_point_dx;
   dg_eval.m_uv = i_dg.m_uv+i_dg.m_duv_dx;
-  Point3D_d displaced_x = dg_eval.m_point + dg_eval.m_geometric_normal*mp_bump_map->Evaluate(dg_eval, i_triangle_index);
+  Point3D_d displaced_x = dg_eval.m_point + i_dg.m_shading_normal*mp_bump_map->Evaluate(dg_eval, i_triangle_index);
 
   dg_eval.m_point = i_dg.m_point_dy;
   dg_eval.m_uv = i_dg.m_uv+i_dg.m_duv_dy;
-  Point3D_d displaced_y = dg_eval.m_point + dg_eval.m_geometric_normal*mp_bump_map->Evaluate(dg_eval, i_triangle_index);
+  Point3D_d displaced_y = dg_eval.m_point + i_dg.m_shading_normal*mp_bump_map->Evaluate(dg_eval, i_triangle_index);
 
-  Point3D_d displace_original = i_dg.m_point + i_dg.m_geometric_normal*mp_bump_map->Evaluate(i_dg, i_triangle_index);
+  Point3D_d displace_original = i_dg.m_point + i_dg.m_shading_normal*mp_bump_map->Evaluate(i_dg, i_triangle_index);
 
   o_bumped_dg = i_dg;
   o_bumped_dg.m_shading_normal = Vector3D_d(displaced_x-displace_original)^Vector3D_d(displaced_y-displace_original);
-  if (o_bumped_dg.m_shading_normal.Normalize()==false)
+  o_bumped_dg.m_tangent = (o_bumped_dg.m_shading_normal^i_dg.m_tangent)^o_bumped_dg.m_shading_normal;
+
+  if (o_bumped_dg.m_shading_normal.Normalize()==false || o_bumped_dg.m_tangent.Normalize()==false)
     {
-    o_bumped_dg.m_shading_normal=i_dg.m_shading_normal;
+    o_bumped_dg.m_shading_normal = i_dg.m_shading_normal;
     return;
     }
 
-  // Orient shading normal to match the geometric normal.
+  // Orient shading normal to match the geometric normal (the tangent vector does not need to be adjusted).
   if (i_dg.m_geometric_normal*o_bumped_dg.m_shading_normal < 0.0)
     o_bumped_dg.m_shading_normal *= -1.0;
-
-  // Maybe this needs more intelligent transformation, like a rotation ?..
-  o_bumped_dg.m_shading_normal += i_dg.m_shading_normal-i_dg.m_geometric_normal;
-  o_bumped_dg.m_shading_normal.Normalize();
   }
